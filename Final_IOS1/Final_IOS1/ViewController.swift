d75ffe83fc95ab1f9d9e718af08ab2de2df7c96d
Steven Perrin <spcnn@mail.missouri.edu>
2019-05-10 21:21:41 -0500

    Close

--- Final_IOS1/Final_IOS1/ViewController.swift
+++ Final_IOS1/Final_IOS1/ViewController.swift
@@ -10,12 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var OutputDisplay: UITextField!
+    
+    @IBOutlet weak var OutputDisplay: UILabel!
+    
+    @IBOutlet weak var InputDisplay: UILabel!
+    
+    var NumberShowing: Double = 0
     
     
-    @IBOutlet weak var InputDisplay: UITextField!
-    
-    
+    @IBAction func ClearButton(_ sender: UIButton) {
+        
+    }
     
     
     override func viewDidLoad() {
@@ -23,6 +28,58 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func Numbers(_ sender: UIButton) {
+        
+        InputDisplay.text = InputDisplay.text! + String(sender.tag-1)
+        NumberShowing = Double(InputDisplay.text!)!
+    }
 
+    @IBAction func Converter(_ sender: Any) {
+
+        let alert = UIAlertController(title: "Converter", message: "Choose Converter", preferredStyle: .actionSheet)
+
+        
+        alert.addAction(UIAlertAction(title: "fahrenheit to celcius", style: .default, handler: {
+            (alertAction)  in
+            var convertedNumber = Double(self.InputDisplay.text!)!
+            convertedNumber = convertedNumber * 9/5 + 32
+            self.OutputDisplay.text = "\(convertedNumber)°C"
+        }))
+
+        alert.addAction(UIAlertAction(title: "celcius to fahrenheit", style: .default, handler: {
+            (alertAction) in
+            var convertedNumber = Double(self.InputDisplay.text!)!
+            convertedNumber = convertedNumber * 9/5 - 32
+            self.OutputDisplay.text = "\(convertedNumber)°C"
+        }))
+
+        alert.addAction(UIAlertAction(title: "miles to kilometers", style: .default, handler: {
+            (alertAction) in
+            var convertedNumber = Double(self.InputDisplay.text!)!
+            convertedNumber = convertedNumber * 1.609
+            self.OutputDisplay.text = "\(convertedNumber)km"
+        }))
+
+        alert.addAction(UIAlertAction(title: "kilometers to miles", style: .default, handler: {
+            (alertAction) in
+            var convertedNumber = Double(self.InputDisplay.text!)!
+            convertedNumber = convertedNumber / 1.609
+            self.OutputDisplay.text = "\(convertedNumber)mi"
+        }))
+        
+        alert.addAction(UIAlertAction(title: "clear", style: .default, handler: {
+            (alertAction) in
+            self.InputDisplay.text = ""
+            self.OutputDisplay.text = ""
+        }))
+        present(alert, animated: true, completion: nil)
+
+    }
+    
+    
+    
+    
+    
 }
 
